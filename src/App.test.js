85b5ca10d260cc49fe52b2b4264b8a9838818e4b
omadbek <abdullayevomadbek709@gmail.com>
2024-05-23 16:18:22 +0500

    ci cd added

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import React from "react"; // Add this line if it's not already present
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
